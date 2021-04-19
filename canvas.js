68aad5d8794fb63a78a795e1bb4699c2af24af6d
Shishira4 <shivashishira@gmail.com>
2021-04-19 22:42:25 +0530

    .

--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-/*window.addEventListener("load", () => {
+window.addEventListener("load", () => {
   const canvas1 = document.querySelector("#canvas");
   const ctx = canvas1.getContext("2d");
 
@@ -35,35 +35,12 @@
   canvas1.addEventListener("mousemove", draw);
   //mouse down
 
-  //mouse up*/
-
-window.addEventListener("load", () => {
-
-var canvas = new fabric.Canvas(document.getElementById('#canvas'))
-canvas.isDrawingMode = true;
-canvas.freeDrawingBrush.width = 5;
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
+  //mouse up
 
   // color change
   Array.from(colorClass).forEach((elem) => {
     elem.addEventListener("click", (e) => {
-      canvas.freeDrawingBrush.color= e.target.id;
+      ctx.strokeStyle = e.target.id;
     });
   });
-
-  let isDrawing = false;
-canvas.on('mouse:down', function({e}) {
-  isDrawing = true;
-  onMouseDown(e);
-}).on('mouse:up', function({e}) {
-  isDrawing = false;
-  onMouseUp(e);
-}).on('mouse:move', function({e}) {
-  if (isDrawing) {
-    const pointer = canvas.getPointer(e);
-    drawRealTime(e, pointer);
-  }
 });
-}
-//});
