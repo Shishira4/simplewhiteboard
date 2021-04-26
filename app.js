d95a5e3b799baa670e4271deeda9dfb9284e1fa1
Shishira4 <shivashishira@gmail.com>
2021-04-26 22:34:42 +0530

    .

---
+++ app.js
@@ -0,0 +1,58 @@
+
+var express=require("express");
+var bodyParser=require("body-parser");
+  
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/gfg');
+var db=mongoose.connection;
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function(callback){
+    console.log("connection succeeded");
+})
+  
+var app=express()
+  
+  
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+  
+app.post('/sign_up', function(req,res){
+    var fname = req.body.fname;
+    var lname = req.body.lname;
+
+    var email =req.body.email;
+    var pword = req.body.pword;
+    var pword1 = req.body.pword1;
+
+    //var phone =req.body.phone;
+  
+    var data = {
+        "fname": fname,
+        "lname": lname,
+        "email":email,
+        "password":pword,
+        "password1":pword1
+        //"phone":phone
+    }
+db.collection('details').insertOne(data,function(err, collection){
+        if (err) throw err;
+        console.log("Record inserted Successfully");
+              
+    });
+          
+    return res.redirect('draw.html');
+})
+  
+  
+app.get('/',function(req,res){
+res.set({
+    'Access-control-Allow-Origin': '*'
+    });
+return res.redirect('try.html');
+}).listen(4000)
+  
+  
+console.log("server listening at port 3000");
---
+++ public/canvas.js
@@ -0,0 +1,125 @@
+console.log("I am in canvas.js");
+const canvas = new fabric.Canvas("canvas",{
+  height: window.innerHeight,
+  width: window.innerWidth,
+  selection:false,
+});
+var brush = new fabric.PencilBrush();
+
+/*canvas.freeDrawingBrush.color = getElementByClass("color").onchange = function(){
+  canvas.freeDrawingBrush.color = this.id; 
+};*/
+
+
+
+const toggleMode =(mode) => {
+
+      currentMode = modes.drawing
+      
+      console.log("draw mode on -1");
+      canvas.renderAll()
+  /*if(mode === modes.drawing)
+  {
+    if(currentMode ===modes.drawing)
+    {
+      currentMode = ''
+      canvas.isDrawingMode = false
+      console.log("draw mode on -1");
+      canvas.renderAll()
+    }
+    else
+    {
+      currentMode = modes.drawing
+    }
+  }
+  console.log(mode);*/
+}
+const setcolor=(color) =>{
+
+document.querySelectorAll('.color-btn')
+
+.forEach(colorbtn => {
+
+colorbtn.addEventListener('click', (target) => {
+  console.log("clicked color pallette");
+  //x = console.log(color)
+  
+  brush.color = this.color
+
+})
+document.getElementById("color").onchange = change;
+canvas.renderAll()
+})
+}
+/*const setcolor=(id) =>{
+  if(mousePressed && currentMode === modes.drawing)
+  {
+  if(document.getElementById('black').clicked == true)
+    {   
+        console.log("black");
+        canvas.freeDrawingBrush.color = '#000000'
+        canvas.renderAll()
+    } 
+  else if(document.getElementById('green').clicked == true)
+    {
+        console.log("green");
+        canvas.freeDrawingCursor.color = '#0037FF'
+        canvas.renderAll()
+    }
+      else if(document.getElementById('green').clicked == true)
+    {
+        canvas.freeDrawingBrush.color = '#02C00F'
+        canvas.renderAll()
+}
+  }
+};*/
+const setPanEvents = (canvas)=>{
+
+  canvas.on('mouse:move', (event)=>{
+    mousePressed=true
+    console.log('mouse move');
+    /*if(mousePressed)
+    {
+      canvas.setCursor('crosshair');
+    const mEvent = event.e;
+    const delta = new fabric.Point(mEvent.movementX, mEvent.movementY)
+    
+    }*/
+    if(mousePressed && currentMode === modes.drawing)
+    {
+      canvas.isDrawingMode = true
+      console.log("draw mode on");
+      canvas.setCursor('freeDrawingBrush');
+      canvas.renderAll()
+      
+      }
+    }
+  
+  )
+  
+  //mouse over
+  canvas.on('mouse:down',(event)=>{
+    mousePressed = true;
+    canvas.setCursor('crosshair');
+    //draw()
+  })
+  canvas.on('mouse:up',(event)=>{
+    console.log('mouse down');
+    mousePressed = false;
+    //draw()
+  })
+  
+  canvas.requestRenderAll();
+}
+let mousePressed = false;
+let currentMode;
+const modes = {
+  drawing: 'drawing'
+}
+const color = {
+  black : '#000000',
+  blue : '#0037FF',
+  green : '02C00F'
+}
+setPanEvents(canvas)
+setcolor(canvas)
