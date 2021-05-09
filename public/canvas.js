ec8eb763046547d2cc1533d491d4bb4e6b1a95b1
Shishira4 <shivashishira@gmail.com>
2021-05-09 23:24:21 +0530

    added color and brushes

--- public/canvas.js
+++ public/canvas.js
@@ -4,40 +4,38 @@ const canvas = new fabric.Canvas("canvas",{
   width: window.innerWidth,
   selection:false,
 });
-var brush = new fabric.PencilBrush();
 
+var pencilbrush = new fabric.PencilBrush(canvas);
+var spraybrush = new fabric.SprayBrush(canvas);
+var patternbrush = new fabric.PatternBrush(canvas);
 const toggleMode =(mode) => {
 
-      currentMode = modes.drawing
-      
-      console.log("draw mode on -1");
-      canvas.renderAll()
+  currentMode = modes.drawing
+  
+  //console.log("draw mode on -1");
+  canvas.renderAll()
 }
 const setcolor=(color) =>{
-
-document.querySelectorAll('.color-btn')
-
-.forEach(colorbtn => {
-
-colorbtn.addEventListener('click', (target) => {
-  console.log("clicked color pallette");
-  //x = console.log(color)
-  brush.color = this.String(color);
-canvas.renderAll();
-})
-
-})
+  
+  canvas.freeDrawingBrush.color = color;
+  canvas.renderAll();
+  console.log(color)
 }
-
+const changeBrush=(brush) =>{
+  brush.color = canvas.freeDrawingBrush.color;
+  canvas.freeDrawingBrush = brush;
+  canvas.renderAll();
+  console.log(brush);
+}
 const setPanEvents = (canvas)=>{
 
   canvas.on('mouse:move', (event)=>{
     mousePressed=true
-    console.log('mouse move');
+    //console.log('mouse move');
     if(mousePressed && currentMode === modes.drawing)
     {
       canvas.isDrawingMode = true
-      console.log("draw mode on");
+      //console.log("draw mode on");
       canvas.setCursor('freeDrawingBrush');
       canvas.renderAll()
       
@@ -68,7 +66,9 @@ const modes = {
 const color = {
   black : '#000000',
   blue : '#0037FF',
-  green : '02C00F'
+  green : '#02C00F'
+
+
 }
 document.querySelector('#Download').addEventListener('click', function() {
   html2canvas(document.querySelector('canvas'), {
@@ -79,4 +79,4 @@ document.querySelector('#Download').addEventListener('click', function() {
   });
 });
 setPanEvents(canvas)
-setcolor(canvas)
+
